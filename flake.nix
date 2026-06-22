a7c2defb41c7d079dda343e0e238e5ffe28473cb
Clay M <ClayMolitor@Live.com>
2026-06-22 10:28:28 -0700

    Migrate vim.nix to clix: add neovim with LSP config and clix-vim package

--- flake.nix
+++ flake.nix
@@ -31,25 +31,93 @@
             fi
             tmux split-window -h \; split-window -v \; send-keys -t 2 'nix run nixpkgs#fastfetch -- --logo NixOS --logo-color-1 "#FF0018" --logo-color-2 "#FF8C00" --logo-color-3 "#FFFF41" --logo-color-4 "#008018" --logo-color-5 "#0000F9" --logo-color-6 "#86007D"' C-m \; resize-pane -t 2 -y 31
           '';
+          lsp-servers = with pkgs; [
+            nixd
+            nixfmt
+            nixfmt-tree
+            pyright
+            bash-language-server
+            lua-language-server
+            dockerfile-language-server
+            yaml-language-server
+            taplo
+            terraform-ls
+            systemd-language-server
+          ];
+          clix-neovim = pkgs.neovim.override {
+            configure = {
+              customRC = ''
+                lua << EOF
+                vim.opt.number = true
+                vim.opt.relativenumber = true
+
+                vim.lsp.config('nixd', {
+                  cmd = { 'nixd' },
+                  filetypes = { 'nix' },
+                })
+                vim.lsp.config('pyright', {
+                  cmd = { 'pyright-langserver', '--stdio' },
+                  filetypes = { 'python' },
+                })
+                vim.lsp.config('bashls', {
+                  cmd = { 'bash-language-server', 'start' },
+                  filetypes = { 'sh', 'bash' },
+                })
+                vim.lsp.config('lua_ls', {
+                  cmd = { 'lua-language-server' },
+                  filetypes = { 'lua' },
+                })
+                vim.lsp.config('dockerls', {
+                  cmd = { 'docker-langserver', '--stdio' },
+                  filetypes = { 'dockerfile' },
+                })
+                vim.lsp.config('yamlls', {
+                  cmd = { 'yaml-language-server', '--stdio' },
+                  filetypes = { 'yaml', 'yml' },
+                })
+                vim.lsp.config('taplo', {
+                  cmd = { 'taplo', 'lsp', 'stdio' },
+                  filetypes = { 'toml' },
+                })
+                vim.lsp.config('terraformls', {
+                  cmd = { 'terraform-ls', 'serve' },
+                  filetypes = { 'terraform', 'tf' },
+                })
+                vim.lsp.config('systemd_ls', {
+                  cmd = { 'systemd-language-server' },
+                  filetypes = { 'systemd' },
+                })
+                vim.lsp.enable({ 'nixd', 'pyright', 'bashls', 'lua_ls', 'dockerls', 'yamlls', 'taplo', 'terraformls', 'systemd_ls' })
+                EOF
+              '';
+            };
+          };
+          clix-vim = pkgs.symlinkJoin {
+            name = "clix-vim";
+            paths = [ clix-neovim ] ++ lsp-servers;
+          };
         in
         {
           packages = {
-            inherit tsplit;
+            inherit tsplit clix-vim;
             default = (
               pkgs.symlinkJoin {
                 name = "clix";
                 buildInputs = [ pkgs.makeWrapper ];
-                paths = with pkgs; [
-                  fish
-                  nushell
-                  tmux
-                  tmuxPlugins.better-mouse-mode
-                  bat
-                  broot
-                  tree
-                  pstree
-                  tsplit
-                ];
+                paths =
+                  with pkgs;
+                  [
+                    fish
+                    nushell
+                    tmux
+                    tmuxPlugins.better-mouse-mode
+                    bat
+                    broot
+                    tree
+                    pstree
+                    tsplit
+                  ]
+                  ++ [ clix-vim ];
                 postBuild = ''
                   wrapProgram $out/bin/fish --add-flags "--init-command 'fish_config theme choose Bay\ Cruise'"
                   wrapProgram $out/bin/bat --add-flags "--theme 1337 --style header"
