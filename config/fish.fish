403d37e94d5f5c89f2d1259576f460cc2b64042f
Clay M <ClayMolitor@Live.com>
2026-08-28 20:13:15 -0700

    Fix grep alias, nb alias, direnv hook

--- config/fish.fish
+++ config/fish.fish
@@ -22,10 +22,10 @@ alias vi nvim
 alias cat bat
 alias ls eza
 alias tree 'eza --tree -I ".git"'
-alias grep ripgrep
+alias grep rg
 
 # nix aliases
-alias nb "nix flake update clix --flake ~/nixconf/ && sudo nixos-rebuild switch --flake ~/nixconf/"
+alias nb "sudo nixos-rebuild switch --flake path:/home/clay/nixconf --impure"
 alias fastfetch 'nix run nixpkgs#fastfetch -- --logo NixOS --logo-color-1 "#FF0018" --logo-color-2 "#FF8C00" --logo-color-3 "#FFFF41" --logo-color-4 "#008018" --logo-color-5 "#0000F9" --logo-color-6 "#86007D"'
 
 direnv hook fish | source
